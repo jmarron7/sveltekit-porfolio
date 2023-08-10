c415acc4450dfb7fdc12e6474272d68f4032fb86
Jesus Marron <jesusmarron7@gmail.com>
2023-08-10 06:52:33 -0500

    skills section implemented

--- src/routes/skills/+page.svelte
+++ src/routes/skills/+page.svelte
@@ -9,3 +9,29 @@
 </svelte:head>
 
 <h2 class="text-3xl mb-2 font-semibold">Technologies I Use</h2>
+
+<div class="flex bg-surface-600 hover:bg-primary-500/5 mt-4 rounded p-4">
+	<div class="mr-2"><span class="text-primary-500">■</span> Frontend:</div>
+
+	<div class="flex-1">
+		Svelte/Sveltekit, React, Angular, Bootstrap, Tailwind, ChakraUI, HTML, CSS
+	</div>
+</div>
+
+<div class="flex bg-surface-600 hover:bg-secondary-500/5 mt-4 rounded p-4">
+	<div class="mr-2"><span class="text-secondary-500">■</span> Backend:</div>
+
+	<div class="flex-1">Go, Java, Spring Boot, Python, Typescript</div>
+</div>
+
+<div class="flex bg-surface-600 hover:bg-tertiary-500/5 mt-4 rounded p-4">
+	<div class="mr-2"><span class="text-tertiary-500">■</span> Databases:</div>
+
+	<div class="flex-1">MySQL, PostgreSQL, MongoDB</div>
+</div>
+
+<div class="flex bg-surface-600 hover:bg-error-500/5 mt-4 rounded p-4">
+	<div class="mr-2"><span class="text-error-500">■</span> Other:</div>
+
+	<div class="flex-1">AWS, Azure, Infrastructure-as-Code, REST, gRPC, Git, Windows, Linux</div>
+</div>
