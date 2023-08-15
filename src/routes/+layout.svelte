dba82e5d64ce5e0692a1eb1095f5a9646873ee70
Jesus Marron <jesusmarron7@gmail.com>
2023-08-15 17:01:08 -0500

    gtag added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,16 @@
 <script lang="ts">
-
 	import '../theme.postcss';
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	import '../app.postcss';
 
-	import Header from '$lib/components/Header.svelte'
-	import Footer from '$lib/components/Footer.svelte'
-
+	import Header from '$lib/components/Header.svelte';
+	import Footer from '$lib/components/Footer.svelte';
+	import Analytics from '$lib/components/Analytics.svelte';
 </script>
 
 <main class="max-w-4xl m-auto text-success-50">
 	<Header />
-
+	<Analytics />
 	<div class="px-10">
 		<slot />
 	</div>
