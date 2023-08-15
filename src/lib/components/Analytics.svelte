1bbada2142df969b843db7c3bb35fa1e8c34eeec
Jesus Marron <jesusmarron7@gmail.com>
2023-08-15 17:23:23 -0500

    gTag added

--- src/lib/components/Analytics.svelte
+++ src/lib/components/Analytics.svelte
@@ -1,36 +1,28 @@
-<script setup>
+<script setup lang="ts">
 	import { page } from '$app/stores';
 	const googleAnalyticsID = import.meta.env.VITE_GOOGLE_ANALYTICS_ID;
-
-	if (typeof gtag !== 'undefined') {
-		window.dataLayer = window.dataLayer || [];
-
-		function gtag(...args) {
-			dataLayer.push(...args);
+	$: {
+		if (typeof gtag !== 'undefined') {
+			gtag('config', googleAnalyticsID, {
+				page_title: document.title,
+				page_path: $page.url.pathname
+			});
 		}
-
-		gtag('js', new Date());
-		gtag('config', googleAnalyticsID, {
-			page_title: document.title,
-			page_path: $page.url.pathname
-		});
 	}
 </script>
 
 <svelte:head>
-	<script async src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsID}`}></script>
+	<script async src={'https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsID}'}>
+	</script>
 	<script>
-		if (typeof gtag !== 'undefined') {
-			const googleAnalyticsID = import.meta.env.VITE_GOOGLE_ANALYTICS_ID;
+		const googleAnalyticsID = import.meta.env.VITE_GOOGLE_ANALYTICS_ID;
+		window.dataLayer = window.dataLayer || [];
 
-			window.dataLayer = window.dataLayer || [];
-
-			function gtag(...args) {
-				dataLayer.push(...args);
-			}
-
-			gtag('js', new Date());
-			gtag('config', googleAnalyticsID);
+		function gtag() {
+			dataLayer.push(arguments);
 		}
+
+		gtag('js', new Date());
+		gtag('config', googleAnalyticsID);
 	</script>
 </svelte:head>
